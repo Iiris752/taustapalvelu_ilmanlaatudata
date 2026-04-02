5d51aaca57aba787384e0ea174c0fda57aec994c
Iiris Esmaili <196097075+Iiris752@users.noreply.github.com>
2026-04-02 11:51:59 +0300

    päivän mittaustulokset endpoint

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import psycopg2
 import os
 from dotenv import load_dotenv
+from psycopg2.extras import RealDictCursor
 
 load_dotenv()
 
@@ -16,3 +17,40 @@ def _get_conn():
         host=os.getenv("DB_HOST"),
         port=os.getenv("DB_PORT")
     )
+
+#päivän mittaustulokset:
+@app.get("/measurements/day")
+def get_day_measurements():
+    location_id = request.args.get("location_id")
+    date = request.args.get("date")
+
+    #otetaan measurement taulu ja liitetään sensor tauluun
+    #haetaan vain sille location id:lle kuuluvat rivit
+    #haetaan vain siltä päivältä ja muunnetaan aikaleima pelkäksi päiväksi
+    #sortataan aikajärjestykseen
+    query = """
+        SELECT m.measurement_id, m.sensor_id, s.parameter,
+               m.value, m."datetimeUtc"
+        FROM measurement m
+        JOIN sensor s ON s.sensor_id = m.sensor_id
+        WHERE m.location_id = %s
+          AND m."datetimeUtc"::date = %s
+        ORDER BY m."datetimeUtc"
+    """
+
+    #avataan yhteys tietokantaan ja luodaan kursori ja palautetaan avain-arvo sanakirja muodossa
+    conn = _get_conn()
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+    #suoritetaan SQL kysely parametrisoituna location id ja date
+    #SQL injectionista huolehdittu aiemmin
+    cur.execute(query, (location_id, date))
+    #haetaan SQL rivit
+    rows = cur.fetchall()
+    conn.close()
+
+    #flask muuttaa listan jsoniksi
+    return jsonify(rows)
+
+#ajetaan:
+if __name__ == "__main__":
+    app.run(debug=True)
