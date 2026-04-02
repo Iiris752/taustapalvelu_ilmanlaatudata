11ac6fb859dc2e4428918e4b0fdc9409784810b7
Iiris Esmaili <196097075+Iiris752@users.noreply.github.com>
2026-04-02 11:05:46 +0300

    tietokantayhteyden muodostus

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from flask import Flask
 import psycopg2
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
+app = Flask(__name__)
+
+#tietokantayhteyst
+def _get_conn():
+    return psycopg2.connect(
+        dbname=os.getenv("DB"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT")
+    )
